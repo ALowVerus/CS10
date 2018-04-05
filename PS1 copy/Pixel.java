152337fb7de181f89fd87a512452e13cfa6753d6
Eitan <vilker@Eitans-MacBook-Air.local>
2018-04-05 16:38:01 -0400

    sqwre

---
+++ PS1 copy/Pixel.java
@@ -0,0 +1,22 @@
+
+public class Pixel {
+
+	public Pixel(int x, int y) {
+		this.x = x;
+		this.y = y;
+	}
+	
+	public int getX() {
+		return x;
+	}
+	
+	public in getY() {
+		return y;
+	}
+	
+	public void setPosition(int x, int y) {
+		this.x = x;
+		this.y = y;
+	}
+
+}
